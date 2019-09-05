33c7c637145a2c255ffacb4b38f0d11199312c05
Jonathan Gusler <jmgusler@mail.lipscomb.edu>
2019-09-04 21:18:58 -0500

    fixed a bouncing bug, not sure why it didnt show up before

--- DC_motor_test/DC_motor_test.ino
+++ DC_motor_test/DC_motor_test.ino
@@ -1,15 +1,25 @@
-//DC Motor Control
-
-//The DC motor will begin rotating left and right, and its speed will vary accordingly.
-//Email:support@sunfounder.com
-//Website:www.sunfounder.com
-//2015.5.7
-
+/********************************************************* 
+ *  DC_motor_test
+ *  Jonathan Gusler
+ *  August, 2019
+ *  
+ *  Description: This program controls a motor in two ways.
+ *  The program controls the motors speed and spin direction.
+ *  The default/starting state is off and blowing air
+ *  toward the motor. The yellow button controls the speed, the 
+ *  green button controls the direction. The speed changes from 
+ *  off to high to low to off. Whenever the yellow button is pressed
+ *  the program waits 50ms and then checks if the green button is 
+ *  also pressed. If both are pressed, the motor is reset to off
+ *  and blowing towards the motor.
+ *  
+ *  To handle debouncing, a delay is added after each button press 
+ *  detection to wait before double checking that the button is pressed.
+ *********************************************************/
 void setup()
 {
-Serial.begin(9600);
-pinMode(14,OUTPUT); //initialize the motorIn1 pin as output 
-pinMode(12,OUTPUT); //initialize the motorIn2 pin as output 
+pinMode(14,OUTPUT); //init pin 14 as output 
+pinMode(12,OUTPUT); //init pin 12 as output
 pinMode(13,INPUT);  //init pin 13 as yellow button which changes motor speed
 pinMode(15,INPUT);  //init pin 15 as green button which changes motor direction
 }
@@ -20,118 +30,117 @@ pinMode(15,INPUT);  //init pin 15 as green button which changes motor direction
 
 
 
-//Create variable to keep track of yellow button
+//Create variable to keep track of yellow button, which correlates to the motor speed
 int YellowButtonState = 0;
 
-//Create variable to keep track of green button
+//Create variable to keep track of green button, which correlates to the spin direction
 int GreenButtonState = 0;
 
-//Variable that controls delay in milliseconds to debounce buttons
-int debounce = 250;
-
-  //How to reset motor to off and default direction: press both buttons simultaneously
+//How to reset motor to off and default direction: press both buttons simultaneously
 void MotorReset (){
     YellowButtonState = 0;
     GreenButtonState = 0;
-    delay(debounce); //Debounce.... hopefully
-    Serial.println("Reset");
 }
 
 
 void loop()
 {
   
-  //Keep track of yellow button state
-  if (digitalRead(13) == HIGH && YellowButtonState == 0){
-    delay(50);
-    if (digitalRead(15) == HIGH){
-      MotorReset();
-      return;
+  //This section keeps track of the yellow button 
+  if (digitalRead(13) == HIGH){
+    delay(100); //Makes the program wait before reading again to see if the button
+                // is still pressed, indicating an actual press, not a bounce
+    //Off to High
+    if (digitalRead(13) == HIGH && YellowButtonState == 0){
+      //delay(50);
+      if (digitalRead(15) == HIGH){
+        MotorReset();
+        return;
+      }
+      YellowButtonState = 1;
     }
-    delay(debounce); //Debounce.... hopefully
-    YellowButtonState = 1; //Motor high
-    Serial.println("high");
-  }
-  if (digitalRead(13) == HIGH && YellowButtonState == 1){
-    delay(50);
-    if (digitalRead(15) == HIGH){
-      MotorReset();
-      return;
-    }
-    delay(debounce); //Debounce.... hopefully
-    YellowButtonState = 2; //Motor low
-    Serial.println("low");
-  }
-  if (digitalRead(13) == HIGH && YellowButtonState == 2){
-    delay(50);
-    if (digitalRead(15) == HIGH){
-      MotorReset();
-      return;
-    }
-    delay(debounce); //Debounce.... hopefully
-    YellowButtonState = 0; //Motor off
-    Serial.println("off");
-  }
-
-  //Keep track of green button state
-  if (digitalRead(15) == HIGH && GreenButtonState == 1){
-    delay(50);
-    if (digitalRead(13) == HIGH){
-      MotorReset();
-      return;
-    }
-    delay(debounce); //Debounce.... hopefully
-    GreenButtonState = 0;  //Direction clockwise relative to the motor, blows away from the motor
-    Serial.println("Counterclockwise");
-  }
-  if (digitalRead(15) == HIGH && GreenButtonState == 0){
-    delay(50);
-    if (digitalRead(13) == HIGH){
-      MotorReset();
-      return;
-    }
-    delay(debounce); //Debounce.... hopefully
-    GreenButtonState = 1;  //Direction counterclockwise relative to the motor, blows towards motor
-    Serial.println("Clockwise");
-  }
-
   
+    //High to low
+    if (digitalRead(13) == HIGH && YellowButtonState == 1){
+      //delay(50);
+      if (digitalRead(15) == HIGH){
+        MotorReset();
+        return;
+      }
+      YellowButtonState = 2;
+    }
+  
+    //Low to off
+    if (digitalRead(13) == HIGH && YellowButtonState == 2){
+      //delay(50);
+      if (digitalRead(15) == HIGH){
+        MotorReset();
+        return;
+      }
+      YellowButtonState = 0;
+    }
+  }
+
+  //This section keeps track of the green button state
+  if (digitalRead(15) == HIGH){
+    delay(100); //Makes the program wait before reading again to see if the button
+                // is still pressed, indicating an actual press, not a bounce
+    //Blow away from motor
+    if (digitalRead(15) == HIGH && GreenButtonState == 1){
+      //delay(50);
+      if (digitalRead(13) == HIGH){
+        MotorReset();
+        return;
+      }
+      GreenButtonState = 0;
+    }
+  
+    //Blow towards motor
+    if (digitalRead(15) == HIGH && GreenButtonState == 0){
+      //delay(50);
+      if (digitalRead(13) == HIGH){
+        MotorReset();
+        return;
+      }
+      GreenButtonState = 1;
+    }
+  }
+
+  //This section sends the speed and direction signals to the motor
   if(YellowButtonState == 0){
     if(GreenButtonState == 0){
-    counterclockwise(0);
+      counterclockwise(0);
     }
     else 
-    clockwise(0);
+      clockwise(0);
   }
   if(YellowButtonState == 1){
     if(GreenButtonState == 0){
-    counterclockwise(1000);
+      counterclockwise(1000);
     }
     else 
-    clockwise(1000);
+      clockwise(1000);
   }
   if(YellowButtonState == 2){
     if(GreenButtonState == 0){
-    counterclockwise(500);
+      counterclockwise(500);
     }
     else 
-    clockwise(500);
-  }
-      
-delay(1);
+      clockwise(500);
+  } 
 }
 
 /****************************************/
 //The function to drive motor rotate clockwise
 void clockwise(int Speed)
 {
-  analogWrite(14,Speed); //set the speed of motor
-  analogWrite(12,0); //stop the motorIn2 pin of motor
+  analogWrite(14,Speed); //turn one pin on
+  analogWrite(12,0);     //turn the other pin off
 }
 //The function to drive motor rotate counterclockwise
 void counterclockwise(int Speed)
 {
-  analogWrite(14,0); //stop the motorIn1 pin of motor
-  analogWrite(12,Speed); //set the speed of motor
+  analogWrite(14,0);     //turn one pin off
+  analogWrite(12,Speed); //turn the other pin on
 }
 /****************************************/
